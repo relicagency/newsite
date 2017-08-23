399d6cb95a43eadf338e099eabe57adb795299ad
Seth Sutherland <cokolatepal@gmail.com>
2017-08-23 13:13:27 -0600

    got newsroom up, only page left is TDS

--- dist/bundle.js
+++ dist/bundle.js
@@ -308,7 +308,26 @@
  * Created by Seth on 8/23/2017.
  */
 (function () {
-  angular.module('app').controller('newsroomCtrl', function ($scope, mainService) {});
+    angular.module('app').controller('newsroomCtrl', function ($scope, mainService) {
+
+        var lastRoute = "";
+
+        $scope.newsroomRouteShow = function (num) {
+            TweenMax.to(document.getElementById('newsroom-route-' + num), 0.5, {
+                ease: Power2.easeIn,
+                opacity: 1
+            });
+
+            if (lastRoute !== "" && lastRoute !== num) {
+                TweenMax.to(document.getElementById('newsroom-route-' + lastRoute), 0.5, {
+                    ease: Power2.easeOut,
+                    opacity: 0
+                });
+            }
+
+            lastRoute = num;
+        };
+    });
 })();
 'use strict';
 

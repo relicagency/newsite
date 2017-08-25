660bd21e4064380b52357cf581bb9e294be732b8
Seth Sutherland <cokolatepal@gmail.com>
2017-08-25 09:34:08 -0600

    small adjustment to TDS

--- dist/bundle.js
+++ dist/bundle.js
@@ -102,6 +102,222 @@
 'use strict';
 
 /**
+ * Created by Seth on 8/9/2017.
+ */
+(function () {
+
+    angular.module('app').directive('footerDir', function () {
+        return {
+            restrict: 'E',
+            templateUrl: './directives/footer/footer.html',
+            controller: 'footerCtrl'
+        };
+    });
+})();
+'use strict';
+
+(function () {
+    angular.module('app').controller('footerCtrl', function ($scope) {
+
+        $scope.footerSocialHover = function (social) {
+            TweenMax.fromTo(document.getElementById('footer-social-' + social), 1, { height: "40px", width: "40px" }, { height: "35px", width: "35px" });
+        };
+    });
+})();
+'use strict';
+
+/**
+ * Created by Seth on 8/23/2017.
+ */
+(function () {
+    angular.module('app').directive('getStartedDir', function () {
+        return {
+            restrict: 'E',
+            templateUrl: './directives/get-started/getStarted.html',
+            controller: 'getStartedCtrl'
+        };
+    });
+})();
+'use strict';
+
+/**
+ * Created by Seth on 8/23/2017.
+ */
+(function () {
+    angular.module('app').controller('getStartedCtrl', function ($scope, mainService) {
+
+        $scope.ctaShow = function () {
+            TweenMax.to(document.getElementById('cta-pop-up'), 0.5, {
+                ease: Power2.easeIn,
+                display: 'inline'
+            });
+        };
+
+        $scope.ctaHide = function () {
+            TweenMax.to(document.getElementById('cta-pop-up'), 0.5, {
+                ease: Power2.easeIn,
+                display: 'none'
+            });
+        };
+    });
+})();
+'use strict';
+
+(function () {
+
+    angular.module('app').directive('homeContentDir', function () {
+        return {
+            restrict: 'E',
+            templateUrl: './directives/home-content/home-content.html',
+            controller: 'homeContentCtrl'
+        };
+    });
+})();
+'use strict';
+
+/**
+ * Created by Seth on 8/9/2017.
+ */
+(function () {
+    angular.module('app').controller('homeContentCtrl', function ($scope) {
+
+        var previousContent = "main-content",
+            previousTop = "",
+            mainContent = document.getElementById('main-content'),
+            services = document.getElementById('services'),
+            work = document.getElementById('work'),
+            articles = document.getElementById('articles');
+
+        $scope.switchContent = function (switcher) {
+
+            TweenMax.to(document.getElementById("top-" + switcher), 0.5, { backgroundColor: "white", color: '#161616' });
+            if (previousTop !== "" && previousTop !== "top-" + switcher) {
+                TweenMax.to(document.getElementById(previousTop), 0.5, { backgroundColor: '#161616', color: "white" });
+            }
+
+            TweenMax.to(document.getElementById(previousContent), 0, { display: "none" });
+            TweenMax.to(document.getElementById(switcher), 0, { display: 'flex' });
+
+            previousContent = switcher;
+            previousTop = "top-" + switcher;
+        };
+
+        $scope.switcher = function () {
+            TweenMax.to(document.getElementById(previousContent), 0, { display: "none" });
+            TweenMax.to(document.getElementById('main-content'), 0, { display: "flex" }, 0.5);
+            TweenMax.to(document.getElementById(previousTop), 0.25, { backgroundColor: '#161616', color: "white" });
+
+            previousContent = 'main-content';
+            previousTop = "";
+        };
+
+        $scope.swnContentHover = function (num) {
+
+            if (num === "four") {
+                TweenMax.to(document.getElementById('services-content-' + num), 0.5, { ease: Power2.easeOut, height: "250px" }, 0.25);
+            } else {
+                TweenMax.to(document.getElementById('services-content-' + num), 0.5, { ease: Power2.easeOut, height: "350px" }, 0.25);
+            }
+        };
+
+        $scope.swnContentHoverLeave = function (num) {
+            if (num === "four") {
+                TweenMax.to(document.getElementById('services-content-' + num), 0.25, { ease: Power2.easeOut, height: "200px" });
+            } else {
+                TweenMax.to(document.getElementById('services-content-' + num), 0.25, { ease: Power2.easeOut, height: "300px" });
+            }
+        };
+
+        $scope.clientHover = function (num) {
+            TweenMax.to(document.getElementById('client-image-' + num), 0.5, {
+                height: "225px",
+                width: "225px",
+                ease: Power3.easeOut
+            });
+        };
+        $scope.clientHoverLeave = function (num) {
+            TweenMax.to(document.getElementById('client-image-' + num), 0.25, {
+                height: "200px",
+                width: "200px"
+            });
+        };
+
+        $scope.articleHover = function (num) {
+            TweenMax.to(document.getElementById('article-pic-' + num), 0.5, {
+                height: "225px",
+                width: "225px",
+                ease: Power3.easeOut
+            });
+        };
+        $scope.articleHoverLeave = function (num) {
+            TweenMax.to(document.getElementById('article-pic-' + num), 0.25, {
+                height: "200",
+                width: "200px"
+            });
+        };
+    });
+})();
+'use strict';
+
+(function () {
+    angular.module('app').controller('navCtrl', function ($scope) {
+
+        $scope.navDropper = function (num) {
+
+            TweenMax.to(document.getElementById('nav-item-drop-' + num), 0.4, {
+                ease: Power3.easeIn,
+                display: "flex",
+                height: "500px"
+            });
+
+            TweenMax.to(document.getElementById('nav-item-drop-' + num), 3, {
+                color: "#242424"
+            });
+        };
+
+        $scope.navShrinker = function (num) {
+
+            TweenMax.to(document.getElementById('nav-item-drop-' + num), 0.25, {
+                color: "transparent"
+            });
+            TweenMax.to(document.getElementById('nav-item-drop-' + num), 0.25, {
+                ease: Power3.easeIn,
+                display: "none",
+                height: 0
+            }, 0.25);
+        };
+
+        $scope.dropDownHover = function (num) {
+
+            TweenMax.to(document.getElementById('drop-down-item-' + num), 0.5, {
+                backgroundColor: "#161616",
+                color: "#BD9A35"
+            });
+        };
+
+        $scope.dropDownHoverLeave = function (num) {
+            TweenMax.to(document.getElementById('drop-down-item-' + num), 0.3, {
+                backgroundColor: "white",
+                color: "#242424"
+            });
+        };
+    });
+})();
+'use strict';
+
+(function () {
+
+  angular.module('app').directive('navDir', function () {
+    return {
+      restrict: 'E',
+      templateUrl: './directives/nav/navTmpl.html',
+      controller: 'navCtrl'
+    };
+  });
+})();
+'use strict';
+
+/**
  * Created by Seth on 8/16/2017.
  */
 (function () {
@@ -444,221 +660,5 @@
  */
 (function () {
   angular.module('app').controller('workCtrl', function ($scope, mainService) {});
-})();
-'use strict';
-
-/**
- * Created by Seth on 8/9/2017.
- */
-(function () {
-
-    angular.module('app').directive('footerDir', function () {
-        return {
-            restrict: 'E',
-            templateUrl: './directives/footer/footer.html',
-            controller: 'footerCtrl'
-        };
-    });
-})();
-'use strict';
-
-(function () {
-    angular.module('app').controller('footerCtrl', function ($scope) {
-
-        $scope.footerSocialHover = function (social) {
-            TweenMax.fromTo(document.getElementById('footer-social-' + social), 1, { height: "40px", width: "40px" }, { height: "35px", width: "35px" });
-        };
-    });
-})();
-'use strict';
-
-/**
- * Created by Seth on 8/23/2017.
- */
-(function () {
-    angular.module('app').directive('getStartedDir', function () {
-        return {
-            restrict: 'E',
-            templateUrl: './directives/get-started/getStarted.html',
-            controller: 'getStartedCtrl'
-        };
-    });
-})();
-'use strict';
-
-/**
- * Created by Seth on 8/23/2017.
- */
-(function () {
-    angular.module('app').controller('getStartedCtrl', function ($scope, mainService) {
-
-        $scope.ctaShow = function () {
-            TweenMax.to(document.getElementById('cta-pop-up'), 0.5, {
-                ease: Power2.easeIn,
-                display: 'inline'
-            });
-        };
-
-        $scope.ctaHide = function () {
-            TweenMax.to(document.getElementById('cta-pop-up'), 0.5, {
-                ease: Power2.easeIn,
-                display: 'none'
-            });
-        };
-    });
-})();
-'use strict';
-
-(function () {
-
-    angular.module('app').directive('homeContentDir', function () {
-        return {
-            restrict: 'E',
-            templateUrl: './directives/home-content/home-content.html',
-            controller: 'homeContentCtrl'
-        };
-    });
-})();
-'use strict';
-
-/**
- * Created by Seth on 8/9/2017.
- */
-(function () {
-    angular.module('app').controller('homeContentCtrl', function ($scope) {
-
-        var previousContent = "main-content",
-            previousTop = "",
-            mainContent = document.getElementById('main-content'),
-            services = document.getElementById('services'),
-            work = document.getElementById('work'),
-            articles = document.getElementById('articles');
-
-        $scope.switchContent = function (switcher) {
-
-            TweenMax.to(document.getElementById("top-" + switcher), 0.5, { backgroundColor: "white", color: '#161616' });
-            if (previousTop !== "" && previousTop !== "top-" + switcher) {
-                TweenMax.to(document.getElementById(previousTop), 0.5, { backgroundColor: '#161616', color: "white" });
-            }
-
-            TweenMax.to(document.getElementById(previousContent), 0, { display: "none" });
-            TweenMax.to(document.getElementById(switcher), 0, { display: 'flex' });
-
-            previousContent = switcher;
-            previousTop = "top-" + switcher;
-        };
-
-        $scope.switcher = function () {
-            TweenMax.to(document.getElementById(previousContent), 0, { display: "none" });
-            TweenMax.to(document.getElementById('main-content'), 0, { display: "flex" }, 0.5);
-            TweenMax.to(document.getElementById(previousTop), 0.25, { backgroundColor: '#161616', color: "white" });
-
-            previousContent = 'main-content';
-            previousTop = "";
-        };
-
-        $scope.swnContentHover = function (num) {
-
-            if (num === "four") {
-                TweenMax.to(document.getElementById('services-content-' + num), 0.5, { ease: Power2.easeOut, height: "250px" }, 0.25);
-            } else {
-                TweenMax.to(document.getElementById('services-content-' + num), 0.5, { ease: Power2.easeOut, height: "350px" }, 0.25);
-            }
-        };
-
-        $scope.swnContentHoverLeave = function (num) {
-            if (num === "four") {
-                TweenMax.to(document.getElementById('services-content-' + num), 0.25, { ease: Power2.easeOut, height: "200px" });
-            } else {
-                TweenMax.to(document.getElementById('services-content-' + num), 0.25, { ease: Power2.easeOut, height: "300px" });
-            }
-        };
-
-        $scope.clientHover = function (num) {
-            TweenMax.to(document.getElementById('client-image-' + num), 0.5, {
-                height: "225px",
-                width: "225px",
-                ease: Power3.easeOut
-            });
-        };
-        $scope.clientHoverLeave = function (num) {
-            TweenMax.to(document.getElementById('client-image-' + num), 0.25, {
-                height: "200px",
-                width: "200px"
-            });
-        };
-
-        $scope.articleHover = function (num) {
-            TweenMax.to(document.getElementById('article-pic-' + num), 0.5, {
-                height: "225px",
-                width: "225px",
-                ease: Power3.easeOut
-            });
-        };
-        $scope.articleHoverLeave = function (num) {
-            TweenMax.to(document.getElementById('article-pic-' + num), 0.25, {
-                height: "200",
-                width: "200px"
-            });
-        };
-    });
-})();
-'use strict';
-
-(function () {
-    angular.module('app').controller('navCtrl', function ($scope) {
-
-        $scope.navDropper = function (num) {
-
-            TweenMax.to(document.getElementById('nav-item-drop-' + num), 0.4, {
-                ease: Power3.easeIn,
-                display: "flex",
-                height: "500px"
-            });
-
-            TweenMax.to(document.getElementById('nav-item-drop-' + num), 2, {
-                color: "#242424"
-            });
-        };
-
-        $scope.navShrinker = function (num) {
-
-            TweenMax.to(document.getElementById('nav-item-drop-' + num), 0.25, {
-                color: "transparent"
-            });
-            TweenMax.to(document.getElementById('nav-item-drop-' + num), 0.25, {
-                ease: Power3.easeIn,
-                display: "none",
-                height: 0
-            }, 0.25);
-        };
-
-        $scope.dropDownHover = function (num) {
-
-            TweenMax.to(document.getElementById('drop-down-item-' + num), 0.5, {
-                backgroundColor: "#161616",
-                color: "#BD9A35"
-            });
-        };
-
-        $scope.dropDownHoverLeave = function (num) {
-            TweenMax.to(document.getElementById('drop-down-item-' + num), 0.3, {
-                backgroundColor: "white",
-                color: "#242424"
-            });
-        };
-    });
-})();
-'use strict';
-
-(function () {
-
-  angular.module('app').directive('navDir', function () {
-    return {
-      restrict: 'E',
-      templateUrl: './directives/nav/navTmpl.html',
-      controller: 'navCtrl'
-    };
-  });
 })();
 //# sourceMappingURL=maps/bundle.js.map

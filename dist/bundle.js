02063a5a0201c35fb91ec33f88f1a9b140e39d05
Seth Sutherland <cokolatepal@gmail.com>
2017-08-30 10:53:34 -0600

    home

--- dist/bundle.js
+++ dist/bundle.js
@@ -294,15 +294,24 @@
             mainService.navBackground(offSet);
 
             headlineContent.style.opacity = 1 - csParaStart * 0.0019;
-            headlineContent.style.top = 50 + csParaStart * 0.075 + "%";
-            backgroundGrad.style.opacity = 0.5 - csParaStart * 0.004;
+            if (window.innerWidth > 850) {
+                headlineContent.style.top = 50 + csParaStart * 0.075 + "%";
+            }
+            if (window.innerWidth > 850) {
+                backgroundGrad.style.opacity = 0.5 - csParaStart * 0.004;
+            }if (window.innerWidth < 851) {
+                headlineContent.style.top = 35 + csParaStart * 0.25 + "%";
+                backgroundGrad.style.opacity = 0.5 - csParaStart * 0.4;
+            }
 
             if (window.innerWidth > 1400) {
                 homeMainBack.style.backgroundPositionY = -281 + csParaStart + 'px';
             }if (window.innerWidth < 1400 && window.innerWidth > 1100) {
                 homeMainBack.style.backgroundPositionY = -124 + csParaStart + 'px';
-            }if (window.innerWidth < 1100) {
+            }if (window.innerWidth < 1100 && window.innerWidth > 850) {
                 homeMainBack.style.backgroundPositionY = -50 + csParaStart + 'px';
+            }if (window.innerWidth < 850) {
+                homeMainBack.style.backgroundPositionY = 354 - csParaStart + 'px';
             }
         };
     });

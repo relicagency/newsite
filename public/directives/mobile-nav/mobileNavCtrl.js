055af955df04623eb0aec0859fb40fc86921fc11
Seth <seth@relicagency.com>
2017-09-26 15:56:33 -0600

    mobilenav animation

--- public/directives/mobile-nav/mobileNavCtrl.js
+++ public/directives/mobile-nav/mobileNavCtrl.js
@@ -15,7 +15,14 @@
 
 
 
-            $scope.exitNav = function(){
+            $scope.exitNav = function(section){
+
+                if(section !== "dont") {
+                    TweenMax.to(document.getElementById('mobile-nav-arrow-' + section), 0.05, {
+                        right: 0,
+                        ease: Power2.easeIn
+                    });
+                }
 
             if(trigger){
                 tl.to(hamburgerTwo, 0.08, {top: "47%"})
@@ -32,6 +39,10 @@
                         .to(hamburgerOne, 0.08, { opacity: 1, top: 0})
                         .to(hamburgerTwo, 0.08, { top: "47%"})
                         .to(mobileNav, 0.15, {right: "-101%", ease: Power2.easeOut})
+                        .to(document.getElementById('mobile-nav-arrow-' + section), 0, {right: "100%"})
+                        // .to(document.getElementById('arrow-shaft-' + section), 0, {
+                        //     width: "100%"
+                        // });
             }
 
             trigger = !trigger;

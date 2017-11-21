3ba8e78826eb300f9d577751eeb551d4b3582c33
Seth <seth@relicagency.com>
2017-11-21 16:45:54 -0700

    working on the contact page

--- dist/bundle.js
+++ dist/bundle.js
@@ -844,7 +844,7 @@
             console.info('Response available');
             $scope.response = response;
             if (window.innerWidth < 426) {
-                window.scrollTo(0, 100);
+                window.scrollTo(0, 600);
             }window.pageYOffset = 100;
         };
         $scope.setWidgetId = function (widgetId) {

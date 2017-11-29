db24d57783b093043f240d8c16006379acdea64d
Seth <seth@relicagency.com>
2017-11-29 10:43:19 -0700

    newsroom

--- dist/bundle.js
+++ dist/bundle.js
@@ -1171,39 +1171,108 @@
 
             var awards = document.getElementById('awards'),
                 press = document.getElementById('press'),
-                news = document.getElementById('news');
+                news = document.getElementById('news'),
+                routerAwards = document.getElementById('router-awards'),
+                routerPress = document.getElementById('router-press'),
+                routerNews = document.getElementById('router-news'),
+                blueTriangleAwards = document.getElementById('blue-triangle-awards'),
+                blueTrianglePress = document.getElementById('blue-triangle-press'),
+                blueTriangleNews = document.getElementById('blue-triangle-news');
 
             if (route === "news") {
                 TweenMax.to(news, 0.25, {
                     display: "flex"
                 });
-                TweenMax.to(press, 0.25, {
+                TweenMax.to(routerNews, 0.25, {
+                    backgroundColor: "rgba(255,255,255,0.8)",
+                    color: "#161616"
+                });
+                TweenMax.to(blueTriangleNews, 0.25, {
+                    display: "flex"
+                });
+                TweenMax.to(press, 0.05, {
                     display: "none"
                 });
-                TweenMax.to(awards, 0.25, {
+                TweenMax.to(routerPress, 0.05, {
+                    backgroundColor: "rgba(0,0,0,0.8)",
+                    color: "white"
+                });
+                TweenMax.to(blueTrianglePress, 0.05, {
+                    display: "none"
+                });
+                TweenMax.to(awards, 0.05, {
+                    display: "none"
+                });
+                TweenMax.to(routerAwards, 0.05, {
+                    backgroundColor: "rgba(0,0,0,0.8)",
+                    color: "white"
+                });
+                TweenMax.to(blueTriangleAwards, 0.05, {
                     display: "none"
                 });
             }
             if (route === "awards") {
-                TweenMax.to(press, 0.25, {
+                TweenMax.to(news, 0.05, {
                     display: "none"
                 });
-                TweenMax.to(news, 0.25, {
+                TweenMax.to(routerNews, 0.05, {
+                    backgroundColor: "rgba(0,0,0,0.8)",
+                    color: "white"
+                });
+                TweenMax.to(blueTriangleNews, 0.05, {
+                    display: "none"
+                });
+                TweenMax.to(press, 0.05, {
+                    display: "none"
+                });
+                TweenMax.to(routerPress, 0.05, {
+                    backgroundColor: "rgba(0,0,0,0.8)",
+                    color: "white"
+                });
+                TweenMax.to(blueTrianglePress, 0.05, {
                     display: "none"
                 });
                 TweenMax.to(awards, 0.25, {
                     display: "flex"
+                });
+                TweenMax.to(routerAwards, 0.25, {
+                    backgroundColor: "rgba(255,255,255,0.8)",
+                    color: "#161616"
+                });
+                TweenMax.to(blueTriangleAwards, 0.25, {
+                    display: "inline"
                 });
             }
             if (route === "press") {
-                TweenMax.to(awards, 0.25, {
+                TweenMax.to(news, 0.05, {
                     display: "none"
                 });
-                TweenMax.to(news, 0.25, {
+                TweenMax.to(routerNews, 0.05, {
+                    backgroundColor: "rgba(0,0,0,0.8)",
+                    color: "white"
+                });
+                TweenMax.to(blueTriangleNews, 0.05, {
                     display: "none"
                 });
                 TweenMax.to(press, 0.25, {
                     display: "flex"
+                });
+                TweenMax.to(routerPress, 0.25, {
+                    backgroundColor: "rgba(255,255,255,0.8)",
+                    color: "#161616"
+                });
+                TweenMax.to(blueTrianglePress, 0.25, {
+                    display: "inline"
+                });
+                TweenMax.to(awards, 0.05, {
+                    display: "none"
+                });
+                TweenMax.to(routerAwards, 0.05, {
+                    backgroundColor: "rgba(0,0,0,0.8)",
+                    color: "white"
+                });
+                TweenMax.to(blueTriangleAwards, 0.05, {
+                    display: "none"
                 });
             }
         };

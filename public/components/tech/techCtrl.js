c412ac0f746c67012f20208225310f65d630b5cc
Barry <barrycann@gmail.com>
2017-12-19 13:59:25 -0700

    Fixed the Tech Page

--- public/components/tech/techCtrl.js
+++ public/components/tech/techCtrl.js
@@ -1,6 +1,3 @@
-/**
- * Created by Seth on 8/21/2017.
- */
 (function(){
     angular.module('app')
         .controller('techCtrl', function($scope, mainService){
@@ -14,6 +11,25 @@
                 mainService.navBackground(offSet);
                 mainService.parallaxIt(backgroundPic,csParaStart);
             };
+
+
+            $scope.readMore = function () {
+                TweenMax.to(document.getElementById('tech-more-info'), 0.25, {position: "relative",visibility: "visible",display: "block"});
+                TweenMax.to(document.getElementById('tech-read-more'), 0.25, {visibility: "hidden"});
+              }
+              $scope.readLess = function () {
+                TweenMax.to(document.getElementById('tech-more-info'), 0.25, {position: "absolute",visibility: "hidden"});
+                TweenMax.to(document.getElementById('tech-read-more'), 0.25, {visibility: "visible"});
+                window.scrollTo(0, 0);
+              }
+              $scope.workEnter = function(){
+                let overlay = event.currentTarget.children[0];
+                TweenMax.to(overlay, 0.25, {opacity: 0});
+              }
+              $scope.workLeave = function() {
+                  let overlay = event.currentTarget.children[0];
+                  TweenMax.to(overlay, 0.25, {opacity: 1})
+              }
 
         })
 })();

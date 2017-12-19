cdd220e665c0f50a55c710345b83b1df954be541
Barry <barrycann@gmail.com>
2017-12-19 13:19:23 -0700

    Fixed the Telecom Page

--- public/components/telecom/telecomCtrl.js
+++ public/components/telecom/telecomCtrl.js
@@ -1,90 +1,36 @@
 /**
  * Created by Seth on 8/21/2017.
  */
-(function(){
-    angular.module('app')
-        .controller('telecomCtrl', function($scope, mainService){
+(function () {
+  angular.module('app')
+    .controller('telecomCtrl', function ($scope, mainService) {
 
-            let backgroundPic = document.getElementById('telecom-background');
+      let backgroundPic = document.getElementById('telecom-background');
 
-            window.onscroll = function() {
-                let offSet = window.pageYOffset,
-                    csParaStart = offSet * 0.75;
+      window.onscroll = function () {
+        let offSet = window.pageYOffset,
+          csParaStart = offSet * 0.75;
 
-                mainService.navBackground(offSet);
-                mainService.parallaxIt(backgroundPic,csParaStart);
-            };
+        mainService.navBackground(offSet);
+        mainService.parallaxIt(backgroundPic, csParaStart);
+      };
 
-            $scope.readMore = function () {
-
-                if(window.innerWidth <= 768){
-
-                    TweenMax.to(document.getElementById('telecom-more-info'), 0.25, {
-                        position: "relative",
-                        visibility: "visible",
-                        margin: "0 0 100px 0"
-                    });
-                    TweenMax.to(document.getElementById('telecom-read-more'), 0.25, {
-                        visibility: "hidden"
-                    });
-                    TweenMax.to(document.getElementById('telecom-industry-info'), 0.25, {
-                        margin: "0 0 0 0"
-                    });
-
-                }
-
-                if(window.innerWidth > 768){
-
-                    TweenMax.to(document.getElementById('telecom-more-info'), 0.25, {
-                        position: "relative",
-                        visibility: "visible",
-                        margin: "0 0 100px 0"
-                    });
-                    TweenMax.to(document.getElementById('telecom-read-more'), 0.25, {
-                        visibility: "hidden"
-                    });
-                    TweenMax.to(document.getElementById('telecom-industry-info'), 0.25, {
-                        margin: "100px 0 0 0"
-                    });
-
-                }
-
-            };
-
-            $scope.readLess = function () {
-
-                if(window.innerWidth <= 768){
-                    TweenMax.to(document.getElementById('telecom-more-info'), 0.25, {
-                        position: "absolute",
-                        visibility: "hidden",
-                        margin: "0 0 0 0"
-                    });
-                    TweenMax.to(document.getElementById('telecom-read-more'), 0.25, {
-                        visibility: "visible"
-                    });
-                    TweenMax.to(document.getElementById('telecom-industry-info'), 0.25, {
-                        margin: "0 0 100px 0"
-                    });
-
-                    window.scrollTo(0,0);
-                }
-
-                if(window.innerWidth > 768){
-                    TweenMax.to(document.getElementById('telecom-more-info'), 0.25, {
-                        position: "absolute",
-                        visibility: "hidden",
-                        margin: "0 0 0 0"
-                    });
-                    TweenMax.to(document.getElementById('telecom-read-more'), 0.25, {
-                        visibility: "visible"
-                    });
-                    TweenMax.to(document.getElementById('telecom-industry-info'), 0.25, {
-                        margin: "100px 0 100px 0"
-                    });
-                    window.scrollTo(0,0);
-                }
-
-            }
-
-        })
+      $scope.readMore = function () {
+        TweenMax.to(document.getElementById('telecom-more-info'), 0.25, {position: "relative",visibility: "visible",display: "block"});
+        TweenMax.to(document.getElementById('telecom-read-more'), 0.25, {visibility: "hidden"});
+      }
+      $scope.readLess = function () {
+        TweenMax.to(document.getElementById('telecom-more-info'), 0.25, {position: "absolute",visibility: "hidden"});
+        TweenMax.to(document.getElementById('telecom-read-more'), 0.25, {visibility: "visible"});
+        window.scrollTo(0, 0);
+      }
+      $scope.workEnter = function(){
+        let overlay = event.currentTarget.children[0];
+        TweenMax.to(overlay, 0.25, {opacity: 0});
+      }
+      $scope.workLeave = function() {
+          let overlay = event.currentTarget.children[0];
+          TweenMax.to(overlay, 0.25, {opacity: 1})
+      }
+    })
 })();

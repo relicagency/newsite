00f64785429bcd96df13bdaab4eb11cab3a77600
Barry <barrycann@gmail.com>
2017-12-19 19:12:51 -0700

    Fixed the upper portion of demand gen

--- public/components/demandgen/demandgenCtrl.js
+++ public/components/demandgen/demandgenCtrl.js
@@ -1,20 +1,25 @@
-/**
- * Created by Seth on 8/22/2017.
- */
-(function(){
-    angular.module('app')
-        .controller('demandgenCtrl', function($scope, mainService){
-            
-            let backgroundPic = document.getElementById('demandgen-background');
+(function () {
+	angular.module('app')
+		.controller('demandgenCtrl', function ($scope, mainService) {
+			let backgroundPic = document.getElementById('demandgen-background');
 
-            window.onscroll = function() {
-                let offSet = window.pageYOffset,
-                    csParaStart = offSet * 0.5050505050505050;
+			window.onscroll = function () {
+				let offSet = window.pageYOffset,
+					csParaStart = offSet * 0.5050505050505050;
 
-                mainService.navBackground(offSet);
-                mainService.parallaxIt(backgroundPic,csParaStart);
-            };
+				mainService.navBackground(offSet);
+				mainService.parallaxIt(backgroundPic, csParaStart);
+			};
 
-        })
+			$scope.workEnter = function () {
+				let overlay = event.currentTarget.children[0];
+				TweenMax.to(overlay, 0.25, { opacity: 0 });
+			}
+			$scope.workLeave = function () {
+				let overlay = event.currentTarget.children[0];
+				TweenMax.to(overlay, 0.25, { opacity: 1 })
+			}
+			
+		})
 })();
 
